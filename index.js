6553c49e381c6b58432ccb5265cda8cebaaf6077
georgetech25 <160145301+georgetech25@users.noreply.github.com>
2024-05-13 14:18:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const body = document.querySelector('body');
+
+
+red.addEventListener("click", function(){
+   body.style.background ="red"
+});
+
+
+green.addEventListener("click", function(){
+    body.style.background ="green"
+ });
+
+
+
+
+ blue.addEventListener("click", function(){
+    body.style.background ="blue"
+ });
+
+
+ clr.addEventListener("click", function(){
+    body.style.background =""
+ });
